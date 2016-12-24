3f055491952613b3132b78f0425200544011df77
yajurvendrasinh <ydgohil05@gmail.com>
2016-12-23 19:11:18 -0800

    basic information on Arrays in Javascript

---
+++ array.js
@@ -0,0 +1,15 @@
+/*ARRAY in Javascript*/
+	/*Initializing an array is very simple in Javascript
+	 * one can intialize array using array notation '[]'
+	 * arrays in Javascript are dynamic in nature
+	 * Javascript provides various methods to manipulate arrays.
+	*/
+
+var firstArray = [];
+firstArray.push('Y');
+firstArray.push('A');
+firstArray.push('J');
+firstArray.push('U');
+firstArray.push('R');
+
+console.log(firstArray); // logs out '['Y','A','J','U','R']'
