7e863d1a22729bbb5364cad6d8ade782f3245f67
yajurvendrasinh <ydgohil05@gmail.com>
2016-12-31 16:03:02 -0800

    push linkedList and Dictionary

---
+++ Dictionary.js
@@ -0,0 +1,42 @@
+/* Making Dictionary using arrays
+ * Dictionary is a Key-Value pair Data Structure.
+ * An object in Javascript is impolemented as a key-value pair, but object in its raw form cannot be sorted in Javascript
+ * everything in Javascript is an Object, hence making a dictionary with functionality of sort, arrays can be use
+ * as after all array is an object too in javascript
+ */
+
+ function Dictionay() {
+ 	this.datastore = new Array();
+ 	this.add = add;
+ 	this.find = find;
+ 	this.remove = remove;
+ 	this.showAll = showAll;
+ }
+
+ function add(key, value) {
+ 	this.datastore[key] = value;
+ }
+
+ function find(key) {
+ 	return this.datastore[key];
+ }
+
+ function remove(key) {
+ 	delete this.datastore[key];
+ }
+
+ function showAll() {
+ 	var sortedDict = Object.keys(this.datastore).sort();
+ 	for(var key = 0; key < sortedDict.length; key++) {
+ 		console.log("Key -> " + key + " Value -> " + this.datastore[key]);
+ 	}
+ }
+
+
+/*Example of using the Dictionary class*/
+
+var phoneBook = new Dictionary();
+
+phoneBook.add("Dave", 25);
+phoneBook.add("John", 26);
+phoneBook.showAll();
---
+++ LinkedList.js
@@ -0,0 +1,78 @@
+/*Linked List in Javascript*/
+
+
+/*
+ * Making a CONSTRUCTOR function that sets the value for two properties
+ * this is Node class, which will have properties "element" (that stores node's data) 
+ * and "next" (stores the link for next node)
+ */
+
+function Node(element) {
+	this.element = element;
+	this.next = null;
+}
+
+
+/*
+ * Making LinkedList class which will have functionality/ method of  linked list
+ */
+
+ function LinkedList() {
+ 	this.head = new Node('head');
+ 	this.find = find;
+ 	this.insert = insert;
+ 	this.remove = remove;
+ 	this.show = show;
+ }
+
+ function find() {
+ 	var currentNode = this.head;
+ 	while (currentNode.element != item) {
+ 		currentNode = currentNode.next;
+ 	}
+
+ 	return currentNode;
+ }
+
+ function insert(newElement, item) {
+ 	var newNode = new Node(newElement);
+ 	var current = this.find(item);
+
+ 	newNode.next = current.next;
+ 	current.next = newNode; 
+ }
+
+ function show() {
+ 	var currentNode = this.head;
+
+ 	while(!(currentNode.next == null)) {
+ 		console.log(currentNode.next.element);
+ 		currentNode = currentNode.next;
+ 	}
+ }
+
+ /*Doubly Linked List*/
+
+ function NodeDLL(element) {
+ 	this.element = element;
+ 	this.next = null;
+ 	this.previous = null;
+ }
+
+ function insertDLL(newElement, item) {
+ 	var newNode = new Node(newElement);
+ 	var current = this.find(item);
+ 	newNode.next = current.next;
+ 	newNode.previous = current;
+ 	current.next = newNode;
+ }
+
+ function remove() {
+ 	var currentNode = this.find(item);
+ 	if(!(currentNode.next === null)) {
+ 		currentNode.previous.next = currentNode.next;
+ 		currentNode.next.previous = currentNode.previous;
+ 		currentNode.next = null;
+ 		currentNode.previous = null;
+ 	}
+ }
