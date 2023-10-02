f0a2f21b257faabf90a0eb838176f27badc6b61f
Yaju Gohil <ygohil@roku.com>
2023-10-02 12:19:11 -0700

    10012023

--- JS-Refresher.js
+++ JS-Refresher.js
@@ -120,3 +120,21 @@ function sum(a, b) {
 let sum = function(a, b) {
     return a + b;
   };
+
+  /**
+   * Write the code, one line for each action:
+
+    Create an empty object user.
+    Add the property name with the value John.
+    Add the property surname with the value Smith.
+    Change the value of the name to Pete.
+    Remove the property name from the object.
+   */
+
+    let emptyObj = {};
+    let emptyObjTwo = new Object();
+
+    emptyObj['name'] = "John";
+    emptyObj['name'] = "Pete";
+
+    delete emptyObj['name'];
