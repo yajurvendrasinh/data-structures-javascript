985b305a05c37477f5a51b9ef9b65d189e475161
Yaju Gohil <ygohil@roku.com>
2023-10-28 22:07:35 -0700

    new stuff

---
+++ DOMManipulation/dom.js
@@ -0,0 +1,6 @@
+const grandParent = document.getElementById('grandparent');
+const parent = Array.from(document.getElementsByClassName('parent'));
+
+function changeColor(element) {
+  element.style.backgroundColor = '#000';
+}
---
+++ DeepFlatten.js
@@ -0,0 +1,51 @@
+// Flatten Array
+
+function deepFlattenRecursive(arr) {
+  const flatten = [];
+
+  if (Array.isArray(arr)) {
+    arr.forEach((item) => {
+      const result = deepFlattenRecursive(item);
+      flatten.push(...result);
+    });
+  } else {
+    flatten.push(arr);
+  }
+
+  return flatten;
+}
+
+function deepFlattenIterative(arr) {
+  let stack = [...arr];
+  let flattened = [];
+
+  while (stack.length) {
+    let current = stack.shift();
+    if (Array.isArray(current)) {
+      stack.unshift(...current);
+    } else {
+      flattened.push(current);
+    }
+  }
+  return flattened;
+}
+
+// Flatten Objects
+
+function flattenObj(input) {
+  if (typeof input !== 'object' || typeof input === null) return input;
+  let flattened = {};
+
+  for (let key in input) {
+    let current = input[key];
+    let currentFlat = flattenObj(current);
+
+    if (typeof current === 'object' && typeof current !== null) {
+      flattened = { ...flattened, ...currentFlat };
+    } else {
+      flattened[key] = current;
+    }
+  }
+
+  return flattened;
+}
